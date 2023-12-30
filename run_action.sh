4f5223a6b897c748f83d17952dc16264434b2b76
CarbonSmasher <crabonsmasher@gmail.com>
2023-12-29 19:25:04 -0500

    Attempt at caching

--- run_action.sh
+++ run_action.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo ::group::Build test runner::
-cd /packtest_runner && cargo build
+[[ $1 == build ]] && cd /packtest_runner && cargo build
 cd /github/workspace
 echo ::endgroup::
 /packtest_runner/target/debug/packtest_runner \
-	--comma-separate "$1" \
-	--minecraft-version "$2" \
-	--packtest-url "$3" \
-	--fabric-api-url "$4" \
+	--comma-separate "$2" \
+	--minecraft-version "$3" \
+	--packtest-url "$4" \
+	--fabric-api-url "$5" \
 	--github
